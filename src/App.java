2e7a6e5928a089032b2b14589d2f9a9340d224cb
jasper <whiteana001@warren.k12.in.us>
2024-09-26 13:44:27 -0400

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class App {
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class App {
+    public static void main(String[] args) {
+        LocalDateTime birthday = LocalDateTime.of(2006, 10, 26, 8, 53);
+        ZoneId us = ZoneId.of("America/New_York");
+        ZonedDateTime birthdayZoned = ZonedDateTime.of(birthday, us);
+
+        ZoneId london = ZoneId.of("Europe/London");
+        ZonedDateTime birthdayParis = birthdayZoned.withZoneSameInstant(london);
+        System.out.print(birthdayZoned);
+        System.out.print(birthdayParis);
+    }
 }
